b6f87726e23f36f7d71f78acc70f02e335982684
TTHIRUMA <T-VENGATESHWARAN.THIRUMARAN@CAPGEMINI.COM>
2022-07-07 17:46:31 +0530

    commit

--- FitnessTracker/src/main/java/fitnesstracker/entity/Diet.java
+++ FitnessTracker/src/main/java/fitnesstracker/entity/Diet.java
@@ -6,74 +6,61 @@ import javax.persistence.Id;
 
 @Entity
 public class Diet{
-	
-	@Id
-	@GeneratedValue
-	private int id;
-	private String breakfast;
-	private String dinner;
-	private String snack;
-	private String dietname;
-	public String getDietname() {
-		return dietname;
+@Id
+@GeneratedValue
+		private int id;
+			private String breakfast;
+			private String dinner;
+			private String snack;
+			private String dietname;
+			public String getDietname() {
+				return dietname;
+			}
+			public void setDietname(String dietname) {
+				this.dietname = dietname;
+				this.id = id;
+				this.breakfast = breakfast;
+				this.dinner = dinner;
+				this.snack = snack;
+				this.day = day;
+			}
+			private String day;
+			
+			public int getId() {
+				return id;
+			}
+			public void setId(int id) {
+				this.id = id;
+			}
+			public String getBreakfast() {
+				return breakfast;
+			}
+			public void setBreakfast(String breakfast) {
+				this.breakfast = breakfast;
+			}
+			public String getDinner() {
+				return dinner;
+			}
+			public void setDinner(String dinner) {
+				this.dinner = dinner;
+			}
+			public String getSnack() {
+				return snack;
+			}
+			public void setSnack(String snack) {
+				this.snack = snack;
+			}
+			public String getDay() {
+				return day;
+			}
+			public void setDay(String day) {
+				this.day = day;
+			}
+			@Override
+			public String toString() {
+				return "Diet [id=" + id + ", breakfast=" + breakfast + ", dinner=" + dinner + ", snack=" + snack + ", dietname="
+						+ dietname + ", day=" + day + "]";
+			}
+			
+			
 	}
-	public void setDietname(String dietname) {
-		this.dietname = dietname;
-	}
-	public Diet() {
-		super();
-// TODO Auto-generated constructor stub
-	}
-	public Diet(int id, String breakfast, String dinner, String snack, String day) {
-		super();
-		this.id = id;
-		this.breakfast = breakfast;
-		this.dinner = dinner;
-		this.snack = snack;
-		this.day = day;
-	}
-	private String day;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getBreakfast() {
-		return breakfast;
-	}
-	public void setBreakfast(String breakfast) {
-		this.breakfast = breakfast;
-	}
-	public String getDinner() {
-		return dinner;
-	}
-	public void setDinner(String dinner) {
-		this.dinner = dinner;
-	}
-	public String getSnack() {
-		return snack;
-	}
-	public void setSnack(String snack) {
-		this.snack = snack;
-	}
-	public String getDay() {
-		return day;
-	}
-	public void setDay(String day) {
-		this.day = day;
-	}
-	@Override
-	public String toString() {
-		return "Diet [id=" + id + ", breakfast=" + breakfast + ", dinner=" + dinner + ", snack=" + snack + ", dietname="
-				+ dietname + ", day=" + day + "]";
-	}
-	
-	
-	}
-
-	
- 
-	
-	
--- FitnessTracker/src/main/java/fitnesstracker/repository/DietRepository.java
+++ FitnessTracker/src/main/java/fitnesstracker/repository/DietRepository.java
@@ -1,14 +1,17 @@
 package fitnesstracker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import fitnesstracker.entity.Activity;
 import fitnesstracker.entity.Diet;
 
 @Repository
 public interface DietRepository extends JpaRepository<Diet, String> {
 
+	Diet findBydietName(String dietName);
+
+	
+
+	
 	
 }
